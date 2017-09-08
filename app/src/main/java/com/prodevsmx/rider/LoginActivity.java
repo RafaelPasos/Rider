25febd0f19b02ef2464bd28dfd74278cb39d6b8f
Diego Gustavo Ramirez Guerrero <diegormz111@gmail.com>
2017-09-08 16:09:38 -0500

    Login facebook

---
+++ app/src/main/java/com/prodevsmx/rider/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.prodevsmx.rider;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LoginButton fbLogin;
+    CallbackManager callbackManager;
+    AccessToken token;
+    String [] permissions = {"email", "user_about_me", "user_birthday", "user_events", "user_photos", "public_profile,"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+
+        initializeControls();
+        fbLogin();
+
+    }
+
+    private void initializeControls(){
+
+        callbackManager = CallbackManager.Factory.create();
+        fbLogin = (LoginButton) findViewById(R.id.loginButton);
+
+        fbLogin.setReadPermissions(permissions);
+    }
+
+    private void fbLogin(){
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("loginToken", loginResult.getAccessToken().getToken());
+                token = loginResult.getAccessToken();
+                //TODO: Go to main activity
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("loginToken", "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("loginToken", error.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
--- app/src/main/java/com/prodevsmx/rider/SplashScreenActivity.java
+++ app/src/main/java/com/prodevsmx/rider/SplashScreenActivity.java
@@ -1,13 +1,35 @@
 package com.prodevsmx.rider;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        checkSession();
     }
+
+    private void checkSession(){
+
+        SharedPreferences settings = getSharedPreferences(getString(R.string.strSettings), Context.MODE_PRIVATE);
+
+        boolean bLogin = settings.getBoolean(getString(R.string.strOnLogin), false);
+
+        if(bLogin){
+            //TODO: Go to  main activity
+        }else{
+            Intent goToLogin = new Intent(SplashScreenActivity.this, LoginActivity.class);
+            startActivity(goToLogin);
+            SplashScreenActivity.this.finish();
+        }
+    }
+
 }
