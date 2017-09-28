faf95693169d9d5040efaa075d2ff41bb419e5d4
Pasos <ali.pasos@arusoft.com>
2017-09-28 13:37:53 -0500

    Styles added

--- app/src/main/java/com/prodevsmx/rider/ActivityLand.java
+++ app/src/main/java/com/prodevsmx/rider/ActivityLand.java
@@ -1,10 +1,13 @@
 package com.prodevsmx.rider;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,11 +26,19 @@ public class ActivityLand extends BaseActivity {
     @BindView(R.id.tvEventsNearby)
     TextView tvEventsNearby;
 
+    @BindView(R.id.rider_toolbar)
+    android.support.v7.widget.Toolbar riderToolbar;
+
+    @BindView(R.id.bottomNav)
+    BottomNavigationView navigationBar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
         initializeUIElements();
+
     }
 
     @Override
@@ -35,9 +46,36 @@ public class ActivityLand extends BaseActivity {
         return R.layout.activity_land;
     }
 
+
     public void initializeUIElements(){
         Typeface type = Typeface.createFromAsset(this.getAssets(),"fonts/myraidpro.ttf");
         tvEventsNearby.setTypeface(type);
+
+
+        navigationBar.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navBExplore:
+
+                                break;
+                            case R.id.navBRequest:
+
+                                break;
+                            case R.id.navBVehicles:
+
+                                break;
+                            case R.id.navBPastRides:
+
+                                break;
+                            case R.id.navBNextRides:
+
+                                break;
+                        }
+                        return true;
+                    }
+                });
     }
 
 
