d08e5e577571caa9a45b467c81dfdf821e1da4c4
Pasos <ali.pasos@arusoft.com>
2017-09-29 10:05:41 -0500

    Styled Bottom navigation view to its final form

--- app/src/main/java/com/prodevsmx/rider/ActivityLand.java
+++ app/src/main/java/com/prodevsmx/rider/ActivityLand.java
@@ -1,5 +1,9 @@
 package com.prodevsmx.rider;
 
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.content.res.XmlResourceParser;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +20,7 @@ import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.prodevsmx.rider.utils.BaseActivity;
+import com.prodevsmx.rider.utils.BottomNavigationViewHelper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +32,8 @@ public class ActivityLand extends BaseActivity {
 
     @BindView(R.id.tvEventsNearby)
     TextView tvEventsNearby;
+
+    TextView tvNav;
 
     //@BindView(R.id.rider_toolbar)
     android.support.v7.widget.Toolbar riderToolbar;
@@ -54,10 +61,11 @@ public class ActivityLand extends BaseActivity {
         riderToolbar = findViewById(R.id.rider_toolbar);
         riderToolbar.setTitle("");
         setSupportActionBar(riderToolbar);
-
+        tvNav = findViewById(R.id.tvTestNav);
         navigationBar = findViewById(R.id.bottomNav);
         addBottomNavigationListener();
         navigationBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.disableShiftMode(navigationBar);
         navigationBar.setSelectedItemId(R.id.navBExplore);
     }
 
@@ -67,31 +75,35 @@ public class ActivityLand extends BaseActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navBExplore:
-                        Log.d("crayola", "hola crayola");
+                        Log.d("State", "Explore");
+                        tvNav.setText("3");
                         //TODO: GO TO EXPLORER PAGE
                         return true;
                     case R.id.navBRequest:
                         //TODO: GO TO REQUESTS PAGE
-                        Log.d("crayola", "con la pistola");
+                        tvNav.setText("1");
+                        Log.d("State", "Requests");
                         return true;
                     case R.id.navBVehicles:
                         //TODO: GO TO VEHICLES PAGE
-                        Log.d("crayola", "ruperta y manola");
+                        tvNav.setText("2");
+                        Log.d("State", "Vehicles");
                         return true;
                     case R.id.navBPastRides:
                         //TODO: GO TO PAST RIDES PAGE
-                        Log.d("crayola", "le gusta la trola");
+                        tvNav.setText("4");
+                        Log.d("State", "Past ride");
                         return true;
                     case R.id.navBNextRides:
                         //TODO: GO TO NEXT RIDES PAGE
-                        Log.d("crayola", "si es en barcelona");
+                        tvNav.setText("5");
+                        Log.d("State", "Next rides");
                         return true;
                 }
                 return false;
             }
         };
     }
-
 
 
     @Override
