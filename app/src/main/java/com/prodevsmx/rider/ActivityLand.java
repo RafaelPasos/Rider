6e040755363d2fb35297ab36426ab005eb1e5d6d
Pasos <ali.pasos@arusoft.com>
2017-10-03 12:46:36 -0500

    Added animations to fragments

--- app/src/main/java/com/prodevsmx/rider/ActivityLand.java
+++ app/src/main/java/com/prodevsmx/rider/ActivityLand.java
@@ -4,24 +4,38 @@ import android.app.Fragment;
 import android.content.res.ColorStateList;
 import android.content.res.XmlResourceParser;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.support.transition.TransitionInflater;
+import android.support.transition.TransitionSet;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 import com.prodevsmx.rider.fragments.FragmentExplore;
+import com.prodevsmx.rider.fragments.FragmentNextRides;
+import com.prodevsmx.rider.fragments.FragmentPastRides;
 import com.prodevsmx.rider.fragments.FragmentProfile;
+import com.prodevsmx.rider.fragments.FragmentRequests;
+import com.prodevsmx.rider.fragments.FragmentVehicle;
 import com.prodevsmx.rider.utils.BottomNavigationViewHelper;
+
+import butterknife.ButterKnife;
 
 import static android.R.color.holo_blue_light;
 
 public class ActivityLand extends AppCompatActivity {
+
 
     android.support.v7.widget.Toolbar riderToolbar;
     BottomNavigationView navigationBar;
@@ -49,6 +63,7 @@ public class ActivityLand extends AppCompatActivity {
         BottomNavigationViewHelper.disableShiftMode(navigationBar);
         navigationBar.setSelectedItemId(R.id.navBExplore);
         fragmentManager = getSupportFragmentManager();
+
     }
 
     public void addBottomNavigationListener(){
@@ -57,35 +72,69 @@ public class ActivityLand extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.navBExplore:
-                        changeBottomNavColor(true);
                         Log.d("Fragment", "Explore");
-                        //TODO: GO TO EXPLORER PAGE
+                        changeBottomNavColor(true);
+                        //GO TO EXPLORER PAGE
                         FragmentTransaction transaction = fragmentManager.beginTransaction();
-                        transaction.replace(R.id.fragmentMain, new FragmentExplore());
+                        android.support.v4.app.Fragment fragment = new FragmentExplore();
+                        //fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+                        //fragment.setExitTransition(new Slide(Gravity.LEFT));
+                        fragment.setEnterTransition(new Fade(1));
+                        fragment.setExitTransition(new Fade(2));
+                        transaction.replace(R.id.fragmentMain, fragment);
                         transaction.commit();
                         return true;
                     case R.id.navBRequest:
                         changeBottomNavColor(true);
                         //TODO: GO TO REQUESTS PAGE
-
                         Log.d("Fragment", "Requests");
+                        transaction = fragmentManager.beginTransaction();
+                        fragment = new FragmentRequests();
+                        //fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+                        //fragment.setExitTransition(new Slide(Gravity.LEFT));
+                        fragment.setEnterTransition(new Fade(1));
+                        fragment.setExitTransition(new Fade(2));
+                        transaction.replace(R.id.fragmentMain, fragment);
+                        transaction.commit();
                         return true;
                     case R.id.navBVehicles:
                         changeBottomNavColor(true);
                         //TODO: GO TO VEHICLES PAGE
-
                         Log.d("Fragment", "Vehicles");
+                        transaction = fragmentManager.beginTransaction();
+                        fragment = new FragmentVehicle();
+                        //fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+                        //fragment.setExitTransition(new Slide(Gravity.LEFT));
+                        fragment.setEnterTransition(new Fade(1));
+                        fragment.setExitTransition(new Fade(2));
+                        transaction.replace(R.id.fragmentMain, fragment);
+                        transaction.commit();
                         return true;
                     case R.id.navBPastRides:
                         changeBottomNavColor(true);
                         //TODO: GO TO PAST RIDES PAGE
-
                         Log.d("Fragment", "Past ride");
+                        transaction = fragmentManager.beginTransaction();
+                        fragment = new FragmentPastRides();
+                        //fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+                        //fragment.setExitTransition(new Slide(Gravity.LEFT));
+                        fragment.setEnterTransition(new Fade(1));
+                        fragment.setExitTransition(new Fade(2));
+                        transaction.replace(R.id.fragmentMain, fragment);
+                        transaction.commit();
                         return true;
                     case R.id.navBNextRides:
                         changeBottomNavColor(true);
                         //TODO: GO TO NEXT RIDES PAGE
                         Log.d("Fragment", "Next rides");
+                        transaction = fragmentManager.beginTransaction();
+                        fragment = new FragmentNextRides();
+                        //fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+                        //fragment.setExitTransition(new Slide(Gravity.LEFT));
+                        fragment.setEnterTransition(new Fade(1));
+                        fragment.setExitTransition(new Fade(2));
+                        transaction.replace(R.id.fragmentMain, fragment);
+                        transaction.commit();
                         return true;
                 }
                 return false;
@@ -107,7 +156,12 @@ public class ActivityLand extends AppCompatActivity {
             changeBottomNavColor(false);
             //TODO: GO TO PROFILE PAGE
             FragmentTransaction transaction = fragmentManager.beginTransaction();
-            transaction.replace(R.id.fragmentMain, new FragmentProfile());
+            android.support.v4.app.Fragment fragment = new FragmentProfile();
+            //fragment.setEnterTransition(new Slide(Gravity.RIGHT));
+            //fragment.setExitTransition(new Slide(Gravity.LEFT));
+            fragment.setEnterTransition(new Fade(1));
+            fragment.setExitTransition(new Fade(2));
+            transaction.replace(R.id.fragmentMain, fragment);
             transaction.commit();
             return true;
         }
