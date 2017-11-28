0129da709c02230c796649fc4ac8961b21d901cc
carlosge10 <carlosge10@hotmail.com>
2017-11-28 06:10:38 -0600

    Events from explore fragment populated with real facebook events

--- app/src/main/java/com/prodevsmx/rider/ActivityEventDetail.java
+++ app/src/main/java/com/prodevsmx/rider/ActivityEventDetail.java
@@ -3,47 +3,88 @@ package com.prodevsmx.rider;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.prodevsmx.rider.beans.CarItem;
 import com.prodevsmx.rider.fragments.FragmentExplore;
 import com.prodevsmx.rider.fragments.FragmentVehicle;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-public class ActivityEventDetail extends AppCompatActivity {
+public class ActivityEventDetail extends AppCompatActivity implements OnMapReadyCallback {
 
     String id;
     Spinner carSelect;
+    private FusedLocationProviderClient mFusedLocationClient;
+    LatLng posActual ;
+    TextView locationTV;
+    SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    ArrayList<String> cars;
+    ArrayAdapter<String> adapter;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(20.65, -103.34);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        CameraPosition cp = new CameraPosition.Builder().zoom(15).target(sydney).build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cp));
+    }
+
+    public void updateRoute(LatLng latLng){
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marker in your position"));
+        CameraPosition cp = new CameraPosition.Builder().zoom(15).target(latLng).build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cp));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_detail);
+        cars = new ArrayList<>();
+        posActual = new LatLng(20.65, -103.34);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation();
+        mapFragment =  (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.eventMapFragment);
+        mapFragment.getMapAsync(this);
+
         Bundle b = getIntent().getExtras();
         id = b.getString("id");
         Log.d("idTotomax", id);
-        ArrayList<String> cars = new ArrayList<>();
-        /*Drawable drawable = getResources().getDrawable(R.drawable.fiesta, getTheme());
-        CarItem item = new CarItem(drawable, "Ford Fiesta 2016", "JMH-47-51");
-        cars.add(item);
-        drawable = getResources().getDrawable(R.drawable.porsche, getTheme());
-        item = new CarItem(drawable, "Porsche 911 S", "JNU-17-58");
-        cars.add(item);
-        */
+
         carSelect = (Spinner) findViewById(R.id.spinnerSelectCar);
-        ArrayAdapter<String> adapter;
+
         cars.add("Ford Fiesta 2016");
         cars.add("Porsche 911 S");
         adapter = new ArrayAdapter<String>(getApplicationContext(),
                 android.R.layout.simple_spinner_item, cars);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         carSelect.setAdapter(adapter);
+        locationTV = (TextView) findViewById(R.id.txt_location);
     }
 
     public void selectLocation(View view){
@@ -63,4 +104,29 @@ public class ActivityEventDetail extends AppCompatActivity {
                 break;
         }
     }
+
+    public void getLocation(){
+        try {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    Geocoder geocoder = new Geocoder(ActivityEventDetail.this, Locale.getDefault());
+                    posActual = new LatLng(location.getLatitude(), location.getLongitude());
+                    try {
+                        List<Address> addresses;
+                        addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+                        String address = addresses.get(0).getAddressLine(0);
+                        updateRoute(posActual);
+                        locationTV.setText(posActual.toString());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        catch(SecurityException e) {
+
+        }
+    }
+
 }
